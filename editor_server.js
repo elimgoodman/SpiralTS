1be85a697d14328e879cff7daf271f512b55d985
Eli Goodman <eli.m.goodman@gmail.com>
2012-12-28 16:49:37 -0500

    stuff

---
+++ editor_server.js
@@ -0,0 +1,210 @@
+var __extends = this.__extends || function (d, b) {
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+}
+
+
+var express = require("express")
+var _ = require("underscore")
+var ejs = require("ejs");
+var app = express.createServer();
+app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.static(__dirname + '/public'));
+});
+app.configure('development', function () {
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
+});
+app.configure('production', function () {
+    app.use(express.errorHandler());
+});
+var Fields;
+(function (Fields) {
+    var Field = (function () {
+        function Field(name) {
+            this.name = name;
+        }
+        return Field;
+    })();
+    Fields.Field = Field;    
+    var Str = (function (_super) {
+        __extends(Str, _super);
+        function Str() {
+            _super.apply(this, arguments);
+
+        }
+        return Str;
+    })(Field);
+    Fields.Str = Str;    
+    var URL = (function (_super) {
+        __extends(URL, _super);
+        function URL() {
+            _super.apply(this, arguments);
+
+        }
+        return URL;
+    })(Str);
+    Fields.URL = URL;    
+    var HTML = (function (_super) {
+        __extends(HTML, _super);
+        function HTML() {
+            _super.apply(this, arguments);
+
+        }
+        return HTML;
+    })(Str);
+    Fields.HTML = HTML;    
+})(Fields || (Fields = {}));
+
+var Editors;
+(function (Editors) {
+    var Editor = (function () {
+        function Editor(display_text, value_fn) {
+            this.display_text = display_text;
+            this.value_fn = value_fn;
+        }
+        Editor.prototype.getTemplate = function () {
+            return "none";
+        };
+        Editor.prototype.drawForInstance = function (instance) {
+            return ejs.render(this.getTemplate(), {
+                value: this.value_fn(instance)
+            });
+        };
+        return Editor;
+    })();
+    Editors.Editor = Editor;    
+    var URL = (function (_super) {
+        __extends(URL, _super);
+        function URL() {
+            _super.apply(this, arguments);
+
+        }
+        URL.prototype.getTemplate = function () {
+            return "<input value='<%= value %>'/>";
+        };
+        return URL;
+    })(Editor);
+    Editors.URL = URL;    
+    var HTML = (function (_super) {
+        __extends(HTML, _super);
+        function HTML() {
+            _super.apply(this, arguments);
+
+        }
+        HTML.prototype.getTemplate = function () {
+            return "<textarea>'<%= value %>'</textarea>";
+        };
+        return HTML;
+    })(Editor);
+    Editors.HTML = HTML;    
+})(Editors || (Editors = {}));
+
+var Concepts;
+(function (Concepts) {
+    var Concept = (function () {
+        function Concept(name, display_name, editors, fields, list_label_field) {
+            this.name = name;
+            this.display_name = display_name;
+            this.editors = editors;
+            this.fields = fields;
+            this.list_label_field = list_label_field;
+        }
+        Concept.prototype.getListLabel = function () {
+            return this.display_name;
+        };
+        return Concept;
+    })();
+    Concepts.Concept = Concept;    
+    Concepts.Page = new Concept("page", "Page", [
+        new Editors.URL("The URL", function (page_instance) {
+            return page_instance.get("url");
+        }), 
+        new Editors.HTML("The body:", function (page_instance) {
+            return page_instance.get("body");
+        })
+    ], [
+        new Fields.URL("url"), 
+        new Fields.HTML("body")
+    ], "url");
+    var ConceptInstance = (function () {
+        function ConceptInstance(concept, values) {
+            this.concept = concept;
+            this.values = values;
+        }
+        ConceptInstance.prototype.get = function (field) {
+            return this.values[field];
+        };
+        ConceptInstance.prototype.getListLabel = function () {
+            return String(this.get(this.concept.list_label_field));
+        };
+        return ConceptInstance;
+    })();
+    Concepts.ConceptInstance = ConceptInstance;    
+})(Concepts || (Concepts = {}));
+
+var Action = (function () {
+    function Action(name, body) {
+        this.name = name;
+        this.body = body;
+    }
+    return Action;
+})();
+var Project = (function () {
+    function Project(name, concepts) {
+        this.name = name;
+        this.concepts = concepts;
+    }
+    Project.prototype.executeAction = function (action_name) {
+        var action = _.find(this.actions, function (action) {
+            return action.name == action_name;
+        });
+        action.body();
+    };
+    return Project;
+})();
+var project = new Project("My Project", [
+    Concepts.Page
+]);
+var page_instances = [
+    new Concepts.ConceptInstance(Concepts.Page, {
+        url: "/foo/bar",
+        body: "<h1>Hello world!</h1>"
+    })
+];
+var first_instance = page_instances[0];
+project.actions = [
+    new Action("run", function () {
+        var project_app = express.createServer();
+        _.each(page_instances, function (instance) {
+            project_app.get(instance.get('url'), function (req, res) {
+                res.send(instance.get('body'));
+            });
+        });
+        console.log("here");
+        project_app.listen(1234, function () {
+            console.log("Listening on port 1234!");
+        });
+    })
+];
+project.executeAction('run');
+app.get('/', function (req, res) {
+    res.render('index', {
+        project: project,
+        instances: page_instances,
+        first_instance: first_instance,
+        _: _
+    });
+});
+app.listen(3000, function () {
+    console.log("Listening on port %d in %s mode", 3000, app.settings.env);
+});
+exports.App = app;
+
---
+++ editor_server.ts
@@ -0,0 +1,168 @@
+///<reference path='node.d.ts' />
+///<reference path='underscore.d.ts' />
+
+import http = module("http")
+import url = module("url")
+import express = module("express")
+import _ = module("underscore")
+
+var ejs = require("ejs");
+
+var app = express.createServer();
+
+// Configuration
+app.configure(function(){
+ app.set('views', __dirname + '/views');
+ app.set('view engine', 'ejs');
+ app.use(express.bodyParser());
+ app.use(express.methodOverride());
+ app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+ app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+ app.use(express.errorHandler());
+});
+
+
+
+module Fields {
+    export class Field {
+        constructor(public name:string){}
+    }
+
+    export class Str extends Field {}
+    export class URL extends Str {}
+    export class HTML extends Str {}
+}
+
+module Editors {
+    export class Editor {
+        constructor(public display_text: string, public value_fn: (instance: Concepts.ConceptInstance) => any) {}
+        getTemplate(): string {
+            return "none";
+        }
+
+        drawForInstance(instance: Concepts.ConceptInstance): string {
+            return ejs.render(this.getTemplate(), {value: this.value_fn(instance)});
+        }
+    }
+
+    export class URL extends Editor {
+        getTemplate() {
+            return "<input value='<%= value %>'/>";
+        }
+    }
+
+    export class HTML extends Editor {
+        getTemplate() {
+
+            return "<textarea>'<%= value %>'</textarea>";
+        }
+    }
+}
+
+interface Listable {
+    getListLabel(): string;
+}
+
+module Concepts {
+    export class Concept implements Listable {
+        constructor(
+            public name: string, 
+            public display_name: string, 
+            public editors: Editors.Editor[], 
+            public fields: Fields.Field[],
+            public list_label_field:string )
+        {};
+
+        getListLabel() {
+            return this.display_name;
+        }
+    }
+
+    export var Page = new Concept(
+        "page", 
+        "Page", 
+        [new Editors.URL("The URL", function(page_instance: Concepts.ConceptInstance){
+            return page_instance.get("url");
+        }), new Editors.HTML("The body:", function(page_instance: Concepts.ConceptInstance){
+            return page_instance.get("body");
+        })], 
+        [new Fields.URL("url"), new Fields.HTML("body")],
+        "url"
+    );
+
+    export class ConceptInstance implements Listable {
+        constructor(public concept: Concept, private values: any){}
+
+        get(field: string) : any {
+            return this.values[field];
+        }
+
+        getListLabel() {
+            return String(this.get(this.concept.list_label_field));
+        }
+    }
+}
+
+class Action {
+    constructor(public name: string, public body: () => void){}
+}
+
+class Project {
+    public actions: Action[];
+    constructor(public name: string, public concepts: Concepts.Concept[]){}
+    executeAction(action_name:string) {
+        var action = _.find(this.actions, function(action:Action){
+            return action.name == action_name;
+        });
+
+        action.body();
+    }
+}
+
+var project = new Project("My Project", [Concepts.Page]);
+
+var page_instances = [new Concepts.ConceptInstance(Concepts.Page, {
+    url: "/foo/bar",
+    body: "<h1>Hello world!</h1>"
+})];
+
+var first_instance = page_instances[0];
+
+project.actions = [
+    new Action("run", function() {
+        var project_app = express.createServer();
+        _.each(page_instances, function(instance: Concepts.ConceptInstance) {
+            project_app.get(instance.get('url'), function(req, res) {
+                res.send(instance.get('body'));
+            });
+        });
+
+        project_app.listen(1234, function(){
+           console.log("Listening on port 1234!");
+        });
+    })
+];
+
+project.executeAction('run');
+
+// Routes
+app.get('/', function(req: express.ExpressServerRequest, res: express.ExpressServerResponse) {
+    res.render('index', {
+        project: project, 
+        instances: page_instances,
+        first_instance: first_instance,
+        _: _
+    });
+});
+
+app.listen(3000, function(){
+   console.log("Listening on port %d in %s mode", 3000, app.settings.env);
+});
+
+export var App = app;
